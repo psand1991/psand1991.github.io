902959f0c9be99cb49d8ae78fc771b26aa4a7f82
psand1991 <psand1991@gmail.com>
2019-02-11 13:34:25 -0600

    added js for keybinding navigation

---
+++ keybindings.js
@@ -0,0 +1,15 @@
+shortcut.add("r",function() {
+	window.location.href = "https://www.reddit.com/";
+});
+shortcut.add("g",function() {
+	window.location.href = "https://github.com/";
+});
+shortcut.add("y",function() {
+	window.location.href = "https://youtube.com/";
+});
+shortcut.add("m",function() {
+	window.location.href = "https://gmail.com/";
+});
+shortcut.add("d",function() {
+	window.location.href = "https://drive.google.com/";
+});
